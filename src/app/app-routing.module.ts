1dab3791f0e165aa13a3c093a8f2e5e314107ad8
DavideZanette <dav.zanette@gmail.com>
2022-06-05 14:46:00 +0200

    Final version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,28 +9,8 @@ import { RsvpComponent } from './pages/rsvp/rsvp.component';
 
 const routes: Routes = [
   {
-    path: 'rsvp',
-    component: RsvpComponent
-  },
-  {
-    path: 'bookingInformation',
-    component: BookingInformationComponent
-  },
-  {
-    path: 'confirmation',
-    component: ConfirmationComponent
-  },
-  {
-    path: 'gifts',
-    component: GiftsComponent
-  },
-  {
     path: 'homePage',
     component: HomePageComponent
-  },
-  {
-    path: 'DBData',
-    component: DbDataComponent
   },
   {
     path: '',   redirectTo: '/homePage', pathMatch: 'full'
